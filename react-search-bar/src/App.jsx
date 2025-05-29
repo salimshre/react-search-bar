52c10fbf11c27a26e49c53d015eb9123492e5e0d
Salim Shrestha <salim9shrestha@gmail.com>
2025-05-29 08:19:36 +0545

    c

---
+++ react-search-bar/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <div>hello world</div>
+    <h1> h1 </h1>
+    </>
+
+  )
+}
+
+
+
+
+export default App
